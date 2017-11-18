b8be0023a1f65e758cb851ad89e182b78ab2ce5f
William Fleming <wpf8741@uncw.edu>
2017-11-18 11:27:56 -0500

    Functions Directory Structure
    Laid out the structure for the Azure Functions.  This includes setting up: directory, linting, and testing.

---
+++ Functions/src/data/index.js
@@ -0,0 +1,10 @@
+/**
+ * The Data function is responsible for receiving data from the
+ * Particle WebHook and storing it in the appropriate SQL Server Table.
+ * @param {Object} context The context object is passed in via the Azure Functions
+ * runtime.  It includes methods for accessing and responding to the user's request.
+ */
+module.exports = function data(context) {
+  return context;
+};
+
